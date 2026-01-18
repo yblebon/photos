3cb8530f25a354c5de67b099c18e6f349196ca63
Yasser <ysr.b.lebon@gmail.com>
2026-01-18 17:07:30 +0100

    update

--- docs/sw.js
+++ docs/sw.js
@@ -1,24 +1,10 @@
 const CACHE_NAME = 'pwa-cache-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/photos.json',
-  'https://unpkg.com/lucide@latest'
-];
+const assets = ['/', '/index.html', '/style.css', '/photos.json', 'https://unpkg.com/lucide@latest'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
 });
