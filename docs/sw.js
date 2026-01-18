c5679480473175c99d5c5b209bb2437aacab48e8
Yasser <ysr.b.lebon@gmail.com>
2026-01-18 16:44:03 +0100

    update photos

---
+++ docs/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/photos.json',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
